2743ce43797da3f7040f0aca7018e36f0955ab5e
이슬비 <nina8598@fnfcorp.com>
2026-06-05 13:26:34 +0900

    로그인, 화면 연결, Swagger 추가

---
+++ src/main/java/com/fnf/incident/WebConfig.java
@@ -0,0 +1,16 @@
+package com.fnf.incident;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")        // /api로 시작하는 모든 통로에 대해
+                .allowedOrigins("*")           // 어떤 화면에서 불러도 허용
+                .allowedMethods("*");          // GET, POST, DELETE 등 모두 허용
+    }
+}
